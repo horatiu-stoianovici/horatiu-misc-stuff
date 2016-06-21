67a993d65d1f573da2ca8955f23ab4a7c6f5a37a
horatiu-stoianovici <horatiu.stoianovici@gmail.com>
2016-06-21 23:57:21 +0100

    Did some minor design improvements

--- web-app/betting.py
+++ web-app/betting.py
@@ -6,23 +6,29 @@ import json
 class BettingHandler(webapp2.RequestHandler):
     def get(self):
     	opportunities = json.loads(MiscData.query(MiscData.key == 'latest-betting-opportunities').fetch(1)[0].value)
-        self.response.write("""<html><head><title>Betting opportunities</title><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-        	<script src="https://cdn.rawgit.com/MikeMcl/big.js/master/big.min.js"></script></head><body>""")
+        self.response.write("""<html>
+            <head>
+                <title>Betting opportunities</title>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+                <script src="https://cdn.rawgit.com/MikeMcl/big.js/master/big.min.js"></script>
+                <link rel="stylesheet" href="/static/css/betting.css" />
+            </head>
+            <body>""")
         self.response.write('<h1>Betting opportunities:</h1>')
 
         self.response.write('<ul>')
         i = 0
         for opportunity in opportunities:
-        	self.response.write('<li>')
+        	self.response.write('<li> <div class="container">')
         	self.response.write('<b>%(name)s</b> - %(rate).2f<span> %%' % {"name" : opportunity["name"], "rate" : opportunity['chances']['rate']})
-        	self.response.write('<p>Back - %(backOdds).2f <a href="%(backUrl)s" target="_blank">%(backUrl)s</a></p>' % opportunity)
-        	self.response.write('<p>Lay - %(layOdds).2f with %(layMoney).2f EUR <a href="%(layUrl)s" target="_blank">%(layUrl)s</a></p>' % opportunity)
+        	self.response.write('<div class="container backBackground">Back - %(backOdds).2f <a href="%(backUrl)s" target="_blank">%(backUrl)s</a></div>' % opportunity)
+        	self.response.write('<div class="container layBackground">Lay - %(layOdds).2f with %(layMoney).2f EUR <a href="%(layUrl)s" target="_blank">%(layUrl)s</a></div>' % opportunity)
         	self.response.write("""<input type="text" class="moneyInput" data-index="%(index)d"/> - Back 
         		<span id="back%(index)d" class="backSuggestedAmount"></span> EUR and lay 
         		<span id="lay%(index)d" class="laySuggestedAmount"></span> EUR (with liability of 
         		<span id="liability%(index)s" class="liabilitySuggestedAmount"></span>) and win 
         		<span id="win%(index)s" class="winSuggestedAmount"></span> EUR""" % {"index": i})
-        	self.response.write('</li>')
+        	self.response.write('</div></li>')
         	i += 1
         self.response.write('</ul>')
         self.response.write('<script>bettingOpportunities = ' + json.dumps(opportunities) + ';</script>')
@@ -47,6 +53,7 @@ class BettingHandler(webapp2.RequestHandler):
         			$('#liability' + index).text(layLiability.toFixed(2));
         		};
         		$('.moneyInput').change(recalculateFunc).keyup(recalculateFunc);
+        		$('.moneyInput').val(100).change();
         	</script>
         """);
         self.response.write('</body></html>')
